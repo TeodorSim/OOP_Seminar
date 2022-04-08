1ebc3839558d2d8607f15b7f1333a643f1f4042f
Teodor Simionescu <simionescu.mails@gmail.com>
2022-04-08 21:12:12 +0300

    Seminar 5 complet

--- OOP5/OOP5/Number.cpp
+++ OOP5/OOP5/Number.cpp
@@ -1,93 +1,241 @@
-#include <cstringt.h>
+#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_CPP_OVERLOAD_STANDARD_NAMES 1
 #include "Number.h"
+#include <string.h>
+#include <iostream>
+using namespace std;
 
-Number::Number(const char* value, int base)
-{
-	baseNumber = base;
-	valueNumber = new char[40];
-	strcpy(valueNumber, value);
+Number::Number(const char* value, int base) {
+	this->value = new char[strlen(value)];
+	memcpy(this->value, value, strlen(value) + 1);
+	this->base = base;
+	isNegative = value[0] == '-';
 }
 
-Number::~Number()
-{
-	delete valueNumber;
-	valueNumber = nullptr;
+Number::~Number() {
 }
 
-void Number::SwitchBase(int newBase)
-{
-	//using ITOA pentru convertire (sau ATOI ->numar in string)
+void Number::SwitchBase(int newBase) {
 
-	int len = strlen(valueNumber);
-	int pow = 1;
-	int num = 0;
-	//to decimal
-	for (int i = len - 1;i >= 0;i--) {
-		int val = 0;
-		int chr = valueNumber[i];
-		if (chr >= '0' && chr <= '9')
-			val = (int)chr - '0';
-		else
-			val = (int)chr - 'A' + 10;
-		//veririficare ca cifra sa fie mai mica decat baza
-		if (val < baseNumber) {
-			num += val * pow;
-			pow = pow * baseNumber;
-		}
+	long long n = convertToBaseTen(value, base);
+	char* newValue = convertToBase(n, newBase);
+
+	value = newValue;
+	base = newBase;
+}
+
+void Number::Print() {
+	for (int i = 0; value[i] != '\0'; i++) {
+		cout << value[i];
+	}
+	cout << "\n";
+}
+
+int Number::GetDigitsCount() {
+	return strlen(value) - (isNegative ? 1 : 0);
+}
+
+int Number::GetBase() {
+	return base;
+}
+
+long long Number::getBaseTen() {
+	long long n = convertToBaseTen(value, base);
+	return isNegative ? -n : n;
+}
+
+
+long long Number::convertToBaseTen(char* s, int base) {
+
+	long long n = 0, p = 1;
+
+	for (int i = strlen(value) - 1; i >= (int)isNegative; i--) {
+		n += normalizeDigit(s[i]) * p;
+		p *= base;
 	}
 
-	//from decimal to new base
-	char* res = new char[40];
-	short space = 0;
-	while (num > 0) {
-		//in c se va face echivalentul in char a modulului
-		char c;
-		int mod = num % newBase;
-		if (mod >= 0 && mod < 9)
-			c = (char)(mod + '0');
-		else
-			c = (char)(mod - 10 + 'A');
-		res[space++] = c;
-		num /= newBase;
+	return n;
+}
+
+char* Number::convertToBase(long long n, int base) {
+
+	char* c = new char[50];
+	int i = 0;
+
+	while (n != 0) {
+		int d = n % base;
+		c[i++] = formalizeDigit(d);
+
+		n /= base;
 	}
-	//reverse the result
-	for (int i = 0;i < strlen(res) / 2;i++) {
-		char a = res[i];
-		res[i] = res[strlen(res) - i];
-		res[strlen(res) - i] = a;
+	if (isNegative) {
+		c[i++] = '-';
 	}
-	strcpy(valueNumber, res);
-	baseNumber = newBase;
+	c[i] = '\0';
 
+	for (int l = 0; l < i / 2; l++) {
+		char aux = c[l];
+		c[l] = c[i - l - 1];
+		c[i - l - 1] = aux;
+	}
+
+	return c;
 }
 
-void Number::Print()
-{
-	printf("n1 has %d digits and it is written in base %d\n", valueNumber, baseNumber);
+int Number::normalizeDigit(char c) {
+	return c >= 'A' ? c - 'A' + 10 : c - '0';
 }
 
-int Number::GetDigitsCount()
-{
-	int count = 0;
-	count = strlen(valueNumber);
-	return count;
+char Number::formalizeDigit(int n) {
+	return n >= 10 ? 'A' + (n - 10) : '0' + n;
 }
 
-char *Number::GetValue()
-{
-	return valueNumber;
+
+Number::Number(const Number& n) {
+	base = n.base;
+	//strcpy(value, n.value);
+	value = n.value;
+	this->isNegative = n.isNegative;
+	// delete n
 }
 
-int Number::GetBase()
-{
-	return baseNumber;
+Number::Number(const Number&& n) {
+	this->base = n.base;
+	//copyString(value, n.value);
+	value = n.value;
+	isNegative = n.isNegative;
 }
 
-void Number::operator+(Number& i)
-{
-	i.valueNumber;
-	itoa
-	if (i.GetBase() > baseNumber) {
-		Number b()
+void copyString(char* a, char* b) {
+	b = new char[strlen(a)];
+	for (int i = 0; a[i] != '\0'; i++) {
+		b[i] = a[i];
 	}
 }
+
+
+Number::Number(const int n) {
+	base = 10;
+	value = toString(n);
+	bool b = n < 0;
+	isNegative = b;
+}
+void Number::operator=(int n) {
+	Number nr(toString(n), 10);
+	nr.SwitchBase(base);
+	*this = nr;
+}
+
+
+char Number::operator[](int i) {
+	return value[i];
+}
+
+Number operator+(const Number& x, const Number& y) {
+	Number a = x;
+	Number b = y;
+	int biggestBase = a.GetBase() > b.GetBase() ? a.GetBase() : b.GetBase();
+
+	long long nr = a.getBaseTen();
+	nr += b.getBaseTen();
+
+	char* c = toString(nr);
+
+	Number newNumber(c, 10);
+	newNumber.SwitchBase(biggestBase);
+
+	return newNumber;
+}
+
+
+Number operator-(const Number& x, const Number& y) {
+	Number a = x;
+	Number b = y;
+	int biggestBase = a.GetBase() > b.GetBase() ? a.GetBase() : b.GetBase();
+
+	long long nr = a.getBaseTen();
+	nr -= b.getBaseTen();
+
+	char* c = toString(nr);
+
+	Number newNumber(c, 10);
+
+	newNumber.SwitchBase(biggestBase);
+
+	return newNumber;
+}
+
+bool Number::operator>(Number& n) {
+	long long n1 = getBaseTen();
+	long long n2 = n.getBaseTen();
+
+	return n1 > n2;
+}
+
+void Number::operator=(const char* s) {
+	Number nr(s, 10);
+	*this = nr;
+}
+
+void Number::operator=(const Number& n) {
+	base = n.base;
+	value = n.value;
+	isNegative = n.isNegative;
+}
+
+void Number::operator+=(Number& n) {
+	long long n1 = getBaseTen();
+	n1 += n.getBaseTen();
+	bool b = n1 < 0;
+
+	toString(n1);
+
+	value = toString(n1);
+
+	isNegative = b;
+
+	SwitchBase(base);
+}
+
+void Number::operator--() {
+	value++;
+}
+
+void Number::operator--(int) {
+	value[strlen(value) - 1] = '\0';
+}
+
+char* toString(long long n) {
+	char* s = new char[50];
+	int i = 0;
+
+	bool b = false;
+	if (n < 0) {
+		n = -n;
+		b = true;
+	}
+
+	while (n != 0) {
+		int d = n % 10;
+
+		s[i++] = '0' + d;
+
+		n /= 10;
+	}
+
+	if (b) {
+		s[i++] = '-';
+	}
+
+	s[i] = '\0';
+
+	for (int l = 0; l < i / 2; l++) {
+		char aux = s[l];
+		s[l] = s[i - l - 1];
+		s[i - l - 1] = aux;
+	}
+
+	return s;
+}
+
+
--- OOP5/OOP5/Number.h
+++ OOP5/OOP5/Number.h
@@ -1,25 +1,47 @@
-#pragma once
-class Number
+class Number {
 
-{
-	int baseNumber;
-	char* valueNumber;
+private:
+    char* value;
+    int base;
+    bool isNegative;
+
 public:
-	Number(const char* value, int base); // where base is between 2 and 16
-	~Number();
-	//Numebr(&);
-	// Number(&&);
-	// add operators and copy/move constructor
-	void SwitchBase(int newBase);
+    Number(const char* value, int base);
+    ~Number();
 
-	void Print();
 
-	int  GetDigitsCount(); // returns the number of digits for the current number
+    Number(const Number& n);
+    Number(const Number&& n);
+    Number(const int);
 
-	char *GetValue();
+    char operator[] (int i);
+    friend Number operator+ (const Number& x, const Number& y);
+    friend Number operator- (const Number& x, const Number& y);
+    bool operator> (Number& n);
 
-	int  GetBase(); // returns the current base
+    void operator= (const char*);
+    void operator= (const Number& n);
+    void operator= (int n);
 
-	void operator+(Number& i);
+    void operator+= (Number& n);
+
+    void operator-- ();
+    void operator-- (int);
+
+
+    void SwitchBase(int newBase);
+    void Print();
+    int  GetDigitsCount();
+    int  GetBase();
+    long long getBaseTen();
+
+
+private:
+    long long convertToBaseTen(char* s, int base);
+    char* convertToBase(long long n, int base);
+    int normalizeDigit(char c);
+    char formalizeDigit(int n);
 };
 
+char* toString(long long);
+void copyString(char*, char*);
